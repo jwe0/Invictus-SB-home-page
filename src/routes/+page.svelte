438a242506b920bbc04df164a6171666ea1b2937
jwe0 <jwe0@epic.x>
2024-07-01 20:57:16 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "/src/style.css";
+</script>
+
+<head>
+    <title>Invictus Selfbot</title>
+</head>
+
+
+<body>
+    <div class="center">
+        <h1>Invictus Selfbot</h1>
+        <h2>The best free open source selfbot</h2>
+        <h2>Deved by /jwe0</h2>        
+        <div class="center2">
+            <a href="https://github.com/jwe0/Invictus-SB">Download</a>
+        </div>
+    </div>
+</body>
