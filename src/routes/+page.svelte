eaaea4510a7bfc147f80b618bb10f2aa52aba41d
jwe0 <jwe0@epic.x>
2024-07-16 00:02:02 +0100

    Updated ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,37 @@
 
 
 <body>
-    <div class="center">
-        <h1>Invictus Selfbot</h1>
-        <h2>The best free open source selfbot</h2>
-        <h2>Deved by /jwe0</h2>        
-        <div class="center2">
-            <a href="https://github.com/jwe0/Invictus-SB">Download</a>
+    <div class="banner">
+        <h1 class="title">Invictus Selfbot</h1>  
+        <h2>Like Nighty but free</h2>   
+    </div>
+    <h1 style="text-align: center;"><br>Features</h1>
+    <div class="info_box_container">
+        <div class="info_box">
+            <h3>Giveaway sniper</h3>
+            <p>Automatically snipe giveaways even on bots with buttons as well as reactions. Supports custom bots with an easy to understand json format.</p>
         </div>
+        <div class="info_box">
+            <h3>Nitro sniper</h3>
+            <p>Automatically attempt to redeem nitro codes, detects fake codes and doesn't redeem them to prevent api rate limits.</p>
+        </div>
+        <div class="info_box">
+            <h3>Custom event websocket</h3>
+            <p>We detect changes on your account using the Discord websocket API for extra fast detection.</p>
+        </div>
+    </div>
+    <div class="info_box_container">
+        <div class="info_box">
+            <h3>Open Source</h3>
+            <p>The entire bot is fully open source and viewable on GitHub. Feel free to make a pull request if you have ideas.</p>
+        </div>
+        <div class="info_box">
+            <h3>Custom scripts</h3>
+            <p>Want to add extra functionality to your bot? Make your own custom scripts in python and add them to the scripts folder.</p>
+        </div>
+    </div>
+
+    <div class="banner">
+        <a href="https://github.com/jwe0/Invictus-SB">Download</a>
     </div>
 </body>
