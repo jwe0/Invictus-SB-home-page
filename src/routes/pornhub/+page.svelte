f3a7f1b59b7df4dcb7645c3b2b983944dfcc0e42
jwe0 <jwe0@epic.x>
2024-08-07 13:49:49 +0100

    Added a pornhub comment gen

---
+++ src/routes/pornhub/+page.svelte
@@ -0,0 +1,124 @@
+<script>
+    import { page } from '$app/stores';
+    import "/src/view_video_php_files/global-backgrounds.css";
+    import "/src/view_video_php_files/generated-header.css";
+    import "/src/view_video_php_files/video-show-pc.css";
+    import "/src/view_video_php_files/large.css";
+</script>
+
+<head>
+    <title>Pornhub Comment</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <link rel="alternate" hreflang="x-default" href="https://www.pornhub.com/view_video.php?viewkey=66684e72808c6">
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/global-backgrounds.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/generated-header.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/./video-show-pc.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/large.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/ph-icons.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/header-non-critical.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/commons-non-critical.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/modals_commons.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/playlist-base.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/premium/premium-modals.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/pc/onboardingModalFlow/onboardingModalFlow.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/header-non-critical.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/commons-non-critical.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/modals_commons.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/playlist-base.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/premium/premium-modals.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/pc/onboardingModalFlow/onboardingModalFlow.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/htmlPauseRoll/pb_block.css?cache=2024080603'>
+    <link rel='stylesheet' href='https://ei.phncdn.com/www-static/css/htmlPauseRoll/pb_block.css?cache=2024080603'>
+</head>
+
+<body class="logged-out">
+    <div class="wrapper">
+        <div class="container  ">
+            <div id="main-container" class="clearfix">
+                <div id="vpContentContainer">
+                    <div id="hd-leftColVideoPage">
+                        <div id="under-player-comments"
+                            class="video-wrapper js-commentReplacement cmtPrelaodContainer original"
+                            data-preload="https://ei.phncdn.com/www-static/images/ajax-loader-small.gif">
+                            <div id="cmtWrapper" class="videoViewPage">
+                                <div id="cmtContent">
+                                    <div class="commentBlock closestComment commentTag1559885341 clearfix">
+                                        <div class="topCommentBlock clearfix">
+                                            <div class="userWrap">
+                                                <div class="boxUserComments">
+                                                    <a rel="nofollow" class="userLink clearfix"
+                                                        href="https://www.pornhub.com/"
+                                                        data-disable-popover="" data-type="user"
+                                                        data-userid="2503455441">
+                                                        <img class="commentAvatarImg avatarTrigger"
+                                                            src="{$page.url.searchParams.get('avatar')|| "src/view_video_php_files/default.jpg"}"
+                                                            loading="lazy" width="" height="" alt="{$page.url.searchParams.get('username') || 'Avatar'}"
+                                                            title="" data-title="{$page.url.searchParams.get('username') || 'Avatar'}">
+                                                        <div class="avatarPosition"></div>
+                                                    </a>
+                                                    <div class="usernameWrap clearfix" data-type="user"
+                                                        data-userid="2503455441" data-liu-user="0"
+                                                        data-disable-popover="">
+                                                        <div class="avatarPosition"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="usernameWrap clearfix" data-type="user"
+                                                    data-userid="2503455441" data-liu-user="0" data-disable-popover="">
+                                                    <span class="usernameBadgesWrapper"><a data-event="watch_page"
+                                                            data-label="profile_view" rel="nofollow"
+                                                            href="https://www.pornhub.com/users/pornsuchti69"
+                                                            class="gtm-event-watch-page usernameLink">{$page.url.searchParams.get('username') || "John Doe"}</a></span>
+                                                    <div class="avatarPosition"></div>
+                                                </div>
+                                                <div class="date">
+                                                    {$page.url.searchParams.get('timestamp') || 'now'} </div>
+                                            </div>
+                                            <div class="commentMessage">
+                                                <span>{$page.url.searchParams.get('content') || "Hello World"}</span>
+                                                <div class="actionButtonsBlock">
+                                                    <a data-event="watch_page" data-label="comment_upvote"
+                                                        data-title="Vote Up"
+                                                        class="gtm-event-watch-page tooltipTrig float-left commentBtn buttonClass generalBtn thumbsUp thumbs"
+                                                        rel="nofollow"
+                                                        href="https://www.pornhub.com/front/signup?redirect=D1jYmyxAFU4lT0xmLbw6h7%2FWASCfQj%2F5%2BsKtRFzt8SP0iDgXBcJ7K76QwfqqLJLQ6TzuS9Y74tV%2FkqBoZhPwgHdUk0yJ7DPhfSOWRcyVC2bgJ1QddTvtP%2Bd6oks%3D"
+                                                        >
+                                                        <i class="ph-icon-thumb-up"></i>
+                                                    </a>
+                                                    <span
+                                                        class="rightMargin float-left voteTotal thumbsUpVote1559885341 thumbsUpVoteVideo_1559885341">{$page.url.searchParams.get('likes') || 5}</span>
+                                                    <a data-title="Vote Down"
+                                                        class="gtm-event-watch-page tooltipTrig rightMargin float-left commentBtn buttonClass generalBtn thumbsDown thumbs"
+                                                        rel="nofollow" data-event="watch_page"
+                                                        data-label="comment_downvote"
+                                                        href="https://www.pornhub.com/front/signup?redirect=D1jYmyxAFU4lT0xmLbw6h7%2FWASCfQj%2F5%2BsKtRFzt8SP0iDgXBcJ7K76QwfqqLJLQ6TzuS9Y74tV%2FkqBoZhPwgHdUk0yJ7DPhfSOWRcyVC2bgJ1QddTvtP%2Bd6oks%3D"
+                                                        >
+                                                        <i class="ph-icon-thumb-down"></i>
+                                                    </a>
+
+                                                    <div class="rightMargin float-left Reply1559885341 replyBlock">
+                                                        <a class="commentBtn buttonClass generalBtn reply"
+                                                            href="https://www.pornhub.com/front/signup"
+                                                            >
+                                                            <span>• Reply</span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="buttonBlockRight clearfix">
+                                                <button data-title="Report"
+                                                    class="tooltipTrig commentBtn buttonClass generalBtn reportBtn flag float-left">
+                                                    <i class="ph-icon-flag"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="reset"></div>
+            </div>
+        </div>
+    </div>
+</body>
